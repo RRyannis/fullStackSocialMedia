f5f3db714e55ca01bbac56defb7e19bfbe6f3679
RRyannis <137493198+RRyannis@users.noreply.github.com>
2025-11-18 11:26:48 +0200

    nothing really

--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -1,8 +1,37 @@
 import "./update.scss";
+import { useState, useContext } from "react";
 
 const Update = ({ setOpenUpdate }) => {
+
+    const upload = async () => {
+        try {
+          const formData = new FormData();
+          formData.append("file", file);
+          const res = await makeRequest.post("/upload", formData);
+          return res.data;
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+    const [ texts, setTexts ]= useState({
+        name: "",
+        city: "",
+        website: ""
+    });
+
+    const handleChange = (e) => {
+        setTexts((prev) => ({...prev, [e.target.name]: [e.target.value] }));
+    }
     return(
         <div className="update">Update
+            <form>
+                <input type="file" />
+                <input type="file" />
+                <input type="text" name="name" onChange={handleChange}/>
+                <input type="text" name="city" onChange={handleChange}/>
+                <input type="text" name="website" onChange={handleChange}/>
+            </form>
             <button onClick={()=>setOpenUpdate(false)}>X</button>
         </div>
     )
